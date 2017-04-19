31fb126a9df916df0d7f3ab220283a96767c595b
vinay datta <vpinnaka@islander.tamucc.edu>
2017-04-19 11:27:20 -0500

    1.0 EMILY Toolbar updated also icon repositioned 4-19-2017

--- EMILY/app/src/main/java/android/app/com/emilyrobot/HomeActivity.java
+++ EMILY/app/src/main/java/android/app/com/emilyrobot/HomeActivity.java
@@ -1,8 +1,13 @@
 package android.app.com.emilyrobot;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -17,5 +22,19 @@ public class HomeActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("EMILY");
     }
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.action_bar,menu);
+        /*TextView tv = new TextView(this);
+        tv.setText(getString(R.string.app_name)+"  ");
+       // tv.setTextColor(getResources().getColor(R.color.colorAccent);
+        //tv.setOnClickListener(t);
+        tv.setPadding(5, 0, 5, 0);
+        tv.setTypeface(null, Typeface.BOLD);
+        tv.setTextSize(14);
+        //MenuItem add = menu.add(0, findViewById(R.id.statusTextview), 1, R.string.app_name);//(0,findViewById(R.id.statusTextview),1,R.string.app_name)
+        //add.setActionView(tv).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);*/
+        return super.onCreateOptionsMenu(menu);
+    }
 }
--- EMILY/app/src/main/java/android/app/com/emilyrobot/Mydata.java
+++ EMILY/app/src/main/java/android/app/com/emilyrobot/Mydata.java
@@ -5,11 +5,11 @@ package android.app.com.emilyrobot;
  */
 
 public class Mydata {
-    static String[] nameArray = {"battery", "WIFI", "GPS", "Speed", "Distance", "DistanceWay", "Time TakeOff"};
+    static String[] nameArray = {"100%", "100%", "100%", "0.00m/s", "0.00m", "0.00m", "0:00"};
 
 
 
-    static Integer[] drawableArray = {R.drawable.battery, R.drawable.wifi, R.drawable.gpsreceiving,
+    static Integer[] drawableArray = {R.drawable.battery, R.drawable.signal_strength, R.drawable.gpsreceiving,
             R.drawable.speedometer, R.drawable.home, R.drawable.marker, R.drawable.clock
             };
 
